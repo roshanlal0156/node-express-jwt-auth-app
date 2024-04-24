3780cfb932184acccc4827e47eee07d8f3f41ae8
roshanlal0156 <roshanlal0156@gmail.com>
2024-04-24 17:51:55 +0530

    fixups

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+const port = 9000;
+app.use("/", (req, res) => {
+    res.json({ message: "Hello bro"})
+})
+
+app.listen(9000, () => {
+    console.log(`Starting Server on port ${port}`)
+})
