df0e37f6ad8bb993de46a40b71555eee15385cd3
roshanlal0156 <roshanlal0156@gmail.com>
2024-04-24 18:18:03 +0530

    added views

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-import express from "express";
+const express = require('express');
 
 const app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 const port = 9000;
-app.use("/", (req, res) => {
-    res.json({ message: "Hello bro"})
-})
+// app.use("/", (req, res) => {
+//     res.json({ message: "Hello bro"})
+// })
 
 app.listen(9000, () => {
     console.log(`Starting Server on port ${port}`)
 })
+
+app.get('/', (req, res) => res.render('home'));
